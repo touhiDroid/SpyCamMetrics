6e376da69a81c84a76d6914a74683007da2c34e7
Touhid <tz08128@gmail.com>
2018-03-14 01:50:36 +0600

    vrummmmmmmmmmmmmmmmmmmmmmmm

---
+++ DataParser/src/com/touhidroid/spycam/dataparser/DataParser.kt
@@ -0,0 +1,15 @@
+package com.touhidroid.spycam.dataparser
+
+class DataParser{
+    companion object {
+        private val PACKAGE_NAMES = arrayOf("com.bangladesharmy", "com.imo.android.imoim", "com.whatsapp",
+                "com.facebook.orca", "com.google.android.talk")
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            // for every file in the input-directory
+            // for every package in the package-array
+            // find first occurance of the package-list
+        }
+    }
+}
